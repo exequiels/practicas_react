178ca20bdaa2c7cb073c0eedc5dd9bb7980670ed
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2025-04-15 13:18:34 -0300

    no message

--- 0006-states-arrays/src/components/RemoveChosen.tsx
+++ 0006-states-arrays/src/components/RemoveChosen.tsx
@@ -3,17 +3,22 @@ import { useState } from 'react'
 const RemoveChosen = () => {
   const [initialArray, setInitialArray] = useState(['A', 'B', 'C'])
 
-  function handleRemoveChosen(letraElegida: string) {
+  function handleRemoveChosen(value: string) {
     setInitialArray((currentArray) =>
-      currentArray.filter((letra) => letra !== letraElegida)
+      currentArray.filter((letra) => letra !== value)
     )
   }
 
   return (
     <>
-      Remove Chosen:{' '}
+      <label>Remove Chosen: </label>
       {initialArray.map((letra, index) => (
-        <label key={index} onClick={() => handleRemoveChosen(letra)}>
+        <label
+          key={index}
+          onClick={() => {
+            handleRemoveChosen(letra)
+          }}
+        >
           {letra}
           {index < initialArray.length - 1 && ', '}
         </label>
